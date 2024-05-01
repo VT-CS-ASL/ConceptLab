c9bacc327e768ebe91c624fa2c4c85c40ca6eb2c
sunnyanthony <sunnyanthonycheng@gmail.com>
2024-05-01 13:31:19 -0400

    add new config file

--- search.py
+++ search.py
@@ -8,11 +8,11 @@ for pos_to_neg_loss_factor in np.arange(0.8, 2, 0.2):
         for maxthd in np.arange(0.25, 1, 0.2):
             for minthd in np.arange(0.1, 0.5, 0.1):
                 output = f"{learing_raate}_{maxthd}_{minthd}_{pos_to_neg_loss_factor}"
-                with open('configs/pet_wo_dog_and_cat.yaml', 'r+') as file:
+                with open('configs/new_creative_pet.yaml', 'r+') as file:
                     data = yaml.load(file, Loader=yaml.FullLoader)
                     data['min_cosine_thr'] = minthd
                     data['max_cosine_thr'] = maxthd
                     data['pos_to_neg_loss_factor'] = pos_to_neg_loss_factor
                     data['learing_raate'] = learing_raate
                     yaml.dump(data, file)
-                os.system(f"python -m scripts.train --config configs/pet_wo_dog_and_cat.yaml --output_dir=./{output}")
+                os.system(f"python -m scripts.train --config configs/new_creative_pet.yaml --output_dir=./{output}")
